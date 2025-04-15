400cb9769cb0d897a87a911d8b482a265f4d4bfe
Christian Possidonio <=>
2025-04-15 15:50:23 -0300

    test: teste e2e criação de usuários

---
+++ test/user.e2e-spec.ts
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+import { faker } from '@faker-js/faker';
+
+describe('TaskController (e2e)', () => {
+  let app: INestApplication<App>;
+  let createdUserId: string;
+  // Dados fake
+  const fakeUser = {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+  };
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('POST /users → Criar usuário', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .send(fakeUser)
+      .expect(201);
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.name).toBe(fakeUser.name);
+    createdUserId = response.body.id;
+  });
+});
